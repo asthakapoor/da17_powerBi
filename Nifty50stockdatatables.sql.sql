3cf929a8a2f8bb0a35bc2e98f69b98e2cf4f169b
asthakapoor <asthakapoor62@gmail.com>
2025-05-20 10:18:11 +0530

    All Nifty 50 Stock Data

---
+++ Nifty50stockdatatables.sql.sql
@@ -0,0 +1,403 @@
+--All Nifty50 Stocks--50Tables--
+
+
+
+CREATE TABLE ADANIENT (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE ADANIPORTS (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE APOLLOHOSP (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE ASIANPAINT (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE AXISBANK (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE BAJAJ_AUTO (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE BAJAJFINSV (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE BAJFINANCE (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE BEL (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE BHARTIARTL (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE CIPLA (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE COALINDIA (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE DRREDDY (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE EICHERMOT (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE ETERNAL (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE GRASIM (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE HCLTECH (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE HDFCBANK (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE HDFCLIFE (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE HEROMOTOCO (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE HINDALCO (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE HINDUNILVR (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE ICICIBANK (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE INDUSINDBK (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE INFY (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE ITC (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE JIOFIN (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE JSWSTEEL (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE KOTAKBANK (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE LT (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE M_M (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE MARUTI (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE NESTLEIND (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE NTPC (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE ONGC (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE POWERGRID (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE RELIANCE (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE SBILIFE (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE SBIN (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE SHRIRAMFIN (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE SUNPHARMA (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE TATACONSUM (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE TATAMOTORS (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE TATASTEEL (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE TCS (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE TECHM (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE TITAN (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE TRENT (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE ULTRACEMCO (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
+
+CREATE TABLE WIPRO (
+    date DATE,
+    open FLOAT,
+    ltp FLOAT,
+    volume BIGINT,
+    no_of_trades BIGINT
+);
